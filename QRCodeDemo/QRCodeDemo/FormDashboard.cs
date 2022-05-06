bb1f3b4b312158333b2d5741ed05c0a32eda9c32
JeromeTuble1 <jerometuble80@gmail.com>
2022-05-06 16:06:55 +0800

    initial commit

---
+++ QRCodeDemo/QRCodeDemo/FormDashboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QRCodeDemo
+{
+    public partial class FormDashboard : Form
+    {
+        public FormDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDashboard_Load(object sender, EventArgs e)
+        {
+            MemoryStream mem = new MemoryStream(Class.Picture);
+            pictureBox1.Image = Image.FromStream(mem);
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            FormLogin FL = new FormLogin();
+            this.Hide();
+            FL.ShowDialog();
+        }
+    }
+}
---
+++ QRCodeDemo/QRCodeDemo/FormGenerateQRCode.cs
@@ -0,0 +1,52 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QRCodeDemo
+{
+    public partial class GenerateQRCode : Form
+    {
+        public GenerateQRCode()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGenerateQRCode_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator QG = new QRCodeGenerator();
+            var TubleData = QG.CreateQrCode(textBoxGenerate.Text, QRCodeGenerator.ECCLevel.H);
+            var Tublecode = new QRCode(TubleData);
+            pictureBoxGenerate.Image = Tublecode.GetGraphic(50);
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormDashboard fd = new FormDashboard();
+            fd.ShowDialog();
+        }
+
+        private void buttonSaveAs_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = @"PNG|*.png" })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBoxGenerate.Image.Save(saveFileDialog.FileName);
+                }
+            }
+
+        }
+    }
+}
