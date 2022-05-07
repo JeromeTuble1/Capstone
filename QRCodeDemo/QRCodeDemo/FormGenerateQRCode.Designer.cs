bb887e2dd74a99d426e55ab840334868ae1ba0e6
JeromeTuble1 <jerometuble80@gmail.com>
2022-05-07 12:04:38 +0800

    initial commit

---
+++ QRCodeDemo/QRCodeDemo/FormGenerateQRCode.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace QRCodeDemo
+{
+    partial class GenerateQRCode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxGenerate = new System.Windows.Forms.TextBox();
+            this.pictureBoxGenerate = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGenerate)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonGenerate.FlatAppearance.BorderSize = 0;
+            this.buttonGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGenerate.Location = new System.Drawing.Point(280, 322);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = false;
+            this.buttonGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxGenerate
+            // 
+            this.textBoxGenerate.Location = new System.Drawing.Point(132, 296);
+            this.textBoxGenerate.Name = "textBoxGenerate";
+            this.textBoxGenerate.Size = new System.Drawing.Size(223, 20);
+            this.textBoxGenerate.TabIndex = 1;
+            // 
+            // pictureBoxGenerate
+            // 
+            this.pictureBoxGenerate.Location = new System.Drawing.Point(109, 111);
+            this.pictureBoxGenerate.Name = "pictureBoxGenerate";
+            this.pictureBoxGenerate.Size = new System.Drawing.Size(260, 179);
+            this.pictureBoxGenerate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxGenerate.TabIndex = 2;
+            this.pictureBoxGenerate.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-19, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(519, 100);
+            this.panel1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 46);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Generate QRCode";
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonBack.FlatAppearance.BorderSize = 0;
+            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBack.Location = new System.Drawing.Point(118, 322);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 12;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonSaveAs.FlatAppearance.BorderSize = 0;
+            this.buttonSaveAs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSaveAs.Location = new System.Drawing.Point(199, 321);
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveAs.TabIndex = 13;
+            this.buttonSaveAs.Text = "Save As";
+            this.buttonSaveAs.UseVisualStyleBackColor = false;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
+            // 
+            // GenerateQRCode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 356);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonSaveAs);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBoxGenerate);
+            this.Controls.Add(this.textBoxGenerate);
+            this.Controls.Add(this.buttonGenerate);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "GenerateQRCode";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FormGenerateQRCode_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGenerate)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxGenerate;
+        private System.Windows.Forms.PictureBox pictureBoxGenerate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
